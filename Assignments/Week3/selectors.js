b04d585778324a56b0c76a53feee7ad4ff31c587
TylerTomasello <ttomasel@hawk.iit.edu>
2017-09-11 23:16:17 -0500

    Added the js to change color of each child of relevant

--- Assignments/Week3/selectors.js
+++ Assignments/Week3/selectors.js
@@ -1,7 +1,27 @@
+/*selectors.js file to change the color of the child fields of relevant
+*/
 var main = function () {
   "use strict;"
-
-  $("*").css("color", "red");
-  //$.fadeIn();
+  //change the color for the first child to red and fadeIn
+  $("p:nth-child(1)").css("color", "red");
+  $("p:nth-child(1)").fadeIn();
+  //change the color for the first child to blue and fadeIn
+  $("p:nth-child(2)").css("color", "blue");
+  $("p:nth-child(2)").fadeIn();
+  //change the color for the first child to green and fadeIn
+  $("p:nth-child(3)").css("color", "green");
+  $("p:nth-child(3)").fadeIn();
+  //change the color for the first child to yellow and fadeIn
+  $("p:nth-child(4)").css("color", "yellow");
+  $("p:nth-child(4)").fadeIn();
+  //change the color for the first child to orange and fadeIn
+  $("p:nth-child(5)").css("color", "orange");
+  $("p:nth-child(5)").fadeIn();
+  //change the color for the first child to purple and fadeIn
+  $("p:nth-child(6)").css("color", "purple");
+  $("p:nth-child(6)").fadeIn();
+  //change the color for the first child to pink and fadeIn
+  $("p:nth-child(7)").css("color", "pink");
+  $("p:nth-child(7)").fadeIn();
 };
 $(document).ready(main);
