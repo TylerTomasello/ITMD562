2afb0180fead4321368f8107ec243f0a1598c1d0
TylerTomasello <ttomasel@hawk.iit.edu>
2017-09-18 23:16:54 -0500

    Added the three of a kind and four of a kind

--- Assignments/Week4/poker.js
+++ Assignments/Week4/poker.js
@@ -2,6 +2,7 @@ handAssessor(array){
   var ranks = ["two", "three", "four", "five", "six", "seven", "eight",
                "nine", "ten", "jack", "queen", "king", "ace"];
 
+  //this finds if there is a two pair
   var containsPair = function (hand) {
     var result = false,
     handRanks;
@@ -11,7 +12,44 @@ handAssessor(array){
     });
 
     ranks.forEach(function (rank) {
+      //if there is a two pair
       if (containsNTimes(handRanks, rank, 2)){
+        result = true;
+      }
+    });
+
+    return result;
+  };
+
+  //this finds if there is a three of a kind
+  var containsThreeOfAKind = function (hand) {
+    var result = false,
+    handRanks;
+
+    handRanks = hand.map(function (card) {
+      return card.rank;
+    });
+
+    ranks.forEach(function (rank) {
+      if (containsNTimes(handRanks, rank, 3)){
+        result = true;
+      }
+    });
+
+    return result;
+  };
+
+  //this finds if there is a three of a kind
+  var containsFourOfAKind = function (hand) {
+    var result = false,
+    handRanks;
+
+    handRanks = hand.map(function (card) {
+      return card.rank;
+    });
+
+    ranks.forEach(function (rank) {
+      if (containsNTimes(handRanks, rank, 4)){
         result = true;
       }
     });
