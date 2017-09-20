c9dbc55ad376d26de8d1df20c135e15c8eb9cace
TylerTomasello <ttomasel@hawk.iit.edu>
2017-09-19 20:18:19 -0500

    finish exercises

--- Exercises/Week5/node.js-app/app.js
+++ Exercises/Week5/node.js-app/app.js
@@ -6,12 +6,27 @@ const port = 3000;*/
 const express = require('express')
 const app = express()
 
-	app.get('/', function (req, res) {
-		  res.send('Hello World!')
+var todos = [
+	{
+		"description" : "My first description",
+		"tag" : "first"
+	},
+	{
+		"description" : "My second description",
+		"tag" : "second"
+	}
+]
+
+
+app.get('/todos', function (req, res) {
+	res.json(todos)
 	})
 
-app.post('/', function(req, res) {
-	res.send('You POST to me')
+app.post('/todos', function(req, res) {
+	var newToDo = req.body;
+
+	todos.push(todos)
+	res.json("message" : "You did it")
 })
 
 app.listen(3000, function () {
