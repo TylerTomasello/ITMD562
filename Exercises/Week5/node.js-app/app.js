6425cca068b404b5aff5532a7a21a8334905f0f3
TylerTomasello <ttomasel@hawk.iit.edu>
2017-09-19 19:43:45 -0500

    Express files

--- Exercises/Week5/node.js-app/app.js
+++ Exercises/Week5/node.js-app/app.js
@@ -1,14 +1,19 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3000;*/
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const express = require('express')
+const app = express()
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+	app.get('/', function (req, res) {
+		  res.send('Hello World!')
+	})
+
+app.post('/', function(req, res) {
+	res.send('You POST to me')
+})
+
+app.listen(3000, function () {
+	  console.log('Example app listening on port 3000!')
+	});
