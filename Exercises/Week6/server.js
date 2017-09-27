d38157377aa2710d0596e2e1ac4c50500039796a
TylerTomasello <ttomasel@hawk.iit.edu>
2017-09-26 19:17:15 -0500

    server file

---
+++ Exercises/Week6/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var redis = require('redis');
+var redisClient = redi.createClient();
+
+var counter;
+rdisClient.get("counter", function(err, value){
+  counter = parseInt(value, 10);
+});
+
+http.createServer(function (req, res) {
+  redis.Client
+  res.writeHead(200, {'Content-type': 'text/plain'});
+  res.edn('Hello World!\n Counter: ' + counter);
+
+})
