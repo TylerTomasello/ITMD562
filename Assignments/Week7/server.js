257eeb32610120d2f7a3c14ed14d723dfcfa0b8d
TylerTomasello <ttomasel@hawk.iit.edu>
2017-10-09 18:37:32 -0500

    javascript of gets and posts

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -1,1 +1,19 @@
-var
+const express = require('express')
+const http = require("http")
+const app = express()
+
+app.get('/hands/:handId', function (req, res) {
+  res.send(hand)
+});
+
+/*app.get('/hands/:handId/cards', function (req, res) {
+  res.send('Hello World!')
+});*/
+
+app.post('hands', function (req, res){
+
+})
+
+app.listen(3000, function (){
+  console.log('Example app listening on port 3000')
+});
