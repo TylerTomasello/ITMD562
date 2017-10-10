139881e73a9e332162c5c5ced0f31a6874d16e6f
TylerTomasello <ttomasel@hawk.iit.edu>
2017-10-09 20:08:04 -0500

    javascript to add an object and get it

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -2,15 +2,17 @@ const express = require('express')
 const http = require("http")
 const app = express()
 
+var hand = {"id": "01", "cards": [{"suit": "spades", "rank": "a"}]}
+
 app.get('/hands/:handId', function (req, res) {
-  res.send(hand)
+  res.status(200).send(hand)
 });
 
 /*app.get('/hands/:handId/cards', function (req, res) {
   res.send('Hello World!')
 });*/
 
-app.post('hands', function (req, res){
+app.post('/hands', function (req, res){
 
 })
 
