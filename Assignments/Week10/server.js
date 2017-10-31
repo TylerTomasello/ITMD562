e6aa2a6120b4d8f03be6d76f040b27c49d2fefe9
TylerTomasello <ttomasel@hawk.iit.edu>
2017-10-31 14:12:00 -0500

    fully updated gets and first post to find users

--- Assignments/Week10/server.js
+++ Assignments/Week10/server.js
@@ -35,16 +35,81 @@
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
 app.use(bodyParser.json());
 
+//use mongoose to connect to a database
+mongoose.connect('mongodb://localhost:27017/user');
+
+var userSchema = mongoose.Schema({
+  name : String,
+  email : String,
+  reminder : [{
+    title : String,
+    description : String,
+    created : String
+  }]
+});
+
+var user = mongoose.model('user', userSchema);
+
+
 app.get('/users/:userId', function (req, res) {
+  user.findById(req.params.userId, function (err, user){
+    if (err){
+      res.status(404).send(err);
+    }
+    else {
+      res.status(200).send({
+        'name' : user.name,
+        'email' : user.email
+      });
+    }
+  });
+});
+
+app.get('/users/:userId/reminders', function (req, res) {
   var userId=req.params.userId;
   if(!users[userid-1]){
     res.status(404).send('No user was found with id');
   }
   else{
-    res.status(200).send(users[userId-1]);
+    res.status(200).send(users[userId-1].reminders);
   }
+});
+
+app.get('/users/:userId/reminders/:reminderId', function (req, res) {
+  user.findById(req.params.userId, function (err, user) {
+    if (err){
+      res.status(404).send(err);
+    }
+    else {
+      res.status(200).send({
+        'title' : reminder.title,
+        'description' : reminder.description,
+        'creeated' : reminder.created
+      });
+    }
+  });
+});
+
+app.post('/users', function (req, res) {
+  var new_user=new user({
+    name : req.body.name,
+    email : reeq.body.email
+  });
+  new_user.save(function (err, user) {
+    if (err) {
+      res.status(404).send(err);
+    }
+    else {
+      res.status(200).send(user);
+    }
+  });
+});
+
+app.listen(3000, function (){
+  console.log('User app listening on port 3000');
 });
