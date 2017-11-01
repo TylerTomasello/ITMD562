c83e4f3eb3c6aecede85c01365ac0629f3a79eaa
TylerTomasello <ttomasel@hawk.iit.edu>
2017-11-01 16:17:22 -0500

    fixed all of the get, post and delete because they were not working

--- Assignments/Week10/server.js
+++ Assignments/Week10/server.js
@@ -12,86 +12,57 @@
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//use mongoose to connect to a database
-mongoose.connect('mongodb://localhost:27017/user', {useMongoClient: true});
-
-//make schema to input data
-var userSchema = mongoose.Schema({
-  name : String,
-  email : String,
-  reminder : [{
-    title : String,
-    description : String,
-    created : String
-  }]
-});
-
-var user = mongoose.model('user', userSchema);
-
+var user = [];
 
 //get to find usersId and displaying name and email
 app.get('/users/:userId', function (req, res) {
-  user.findById(req.params.userId, function (err, user){
-    if (err){
-      res.status(404).send(err);
+  var userID = req.params.userId;
+  if (!user[userID-1]){
+    res.status(404).send("User not found for id " + userID);
     }
-    else {
-      res.status(200).send({
-        'name' : user.name,
-        'email' : user.email
-      });
-    }
-  });
+  else {
+    res.status(200).send(user[userID-1].user);
+  }
 });
 
 //get to find reminders of a user and display the reminder title and description
 app.get('/users/:userId/reminders', function (req, res) {
-  user.findById(req.params.userId, 'reminder.title', function(err, user) {
-    if (err) {
-      res.status(404).send(err);
-    }
-    else {
-          res.status(200).send(user.reminder);
-    }
-  });
+  var userID = req.params.userId;
+  var remind = [];
+  if (!user[userID-1]) {
+    res.status(404).send("User not found for id " + userID);
+  }
+  else {
+    users[userID-1].remind.forEach(function (ret) {
+      remind.push(ret.reminder);
+    });
+    res.status(200).send(remind);
+  }
 });
 
 //get to find a specific reminder and display the title description and created
 app.get('/users/:userId/reminders/:reminderId', function (req, res) {
-  user.findById(req.params.userId, function (err, user) {
-    if (err){
-      res.status(404).send(err);
-    }
-    else {
-      res.status(200).send({
-        'title' : reminder.title,
-        'description' : reminder.description,
-        'creeated' : reminder.created
-      });
-    }
-  });
+  var userID = req.params.userId;
+  var reminderID = req.params.reminderId;
+  if (!user[iserID-1].remind[reminderID-1]){
+    res.status(404).send("Reminder not found for id " + reminderID);
+  }
+  else {
+    res.status(200).send(user[userID-1].remind[reminderID-1].reminder);
+  }
 });
 
 //post to create a new user and input the name and email
 app.post('/users', function (req, res) {
-  var new_user=new user({
-    name : req.body.name,
-    email : reeq.body.email
-  });
-  new_user.save(function (err, user) {
-    if (err) {
-      res.status(404).send(err);
-    }
-    else {
-      res.status(200).send(user);
-    }
-  });
+  var id = {'id':user.length+1};
+  var users = req.body;
+  users.id = id.id;
+  user.push(users);
+  res.status(200).send(id);
 });
 
 //post to create a new reminder for a user with a title and description.
@@ -111,80 +82,58 @@ app.post('/users/:userId/reminders', function (req, res) {
   //variable for the timestamp to be used
   var timestamp = date.join("-") + " " + time.join(":") ;
 
-  var new_reminder={
-    title : req.body.title,
-    description : req.body.description,
-    created : timestamp
-  };
+  var userID = req.params.userId;
 
-  user.findById(req.params.userId, function (err, user) {
-    if (err) {
-      res.status(404).send(err);
-    }
-    else {
-      var reminder = user.reminder.create(new_reminder);
-      user.reminder.push(reminder);
-      user.save(function (err, user) {
-        if (err) {
-          res.status(404).send(err);
-        }
-        else {
-          res.status(200).send(reminder);
-        }
-      });
-    }
-  });
+  if (!user[userID-1]) {
+    res.status(404).send("User not found for id " + userID);
+  }
+  else {
+    var id = {'id':user[userID-1].remind.length+1};
+    var new_reminder = req.body;
+    new_reminder.id = id.id;
+    new_reminder.reminder.created = timestamp;
+
+    user[userID-1].remind.push(new_reminder);
+    var reminder = user.reminder.create(new_reminder);
+    res.status(200).send(id);
+  }
 });
 
 //delete to remove the user and all reminders that go with it
 app.delete('/users/:userId', function (req,res){
-  user.remove({_id : req.params.userId}, function (err, user) {
-    if (err) {
-      res.status(404).send(err);
-    }
-    else {
-      res.status(204).send({message : 'User deleted'});
-    }
-  });
+  var userID = req.params.userId;
+  if (!user[userID-1]) {
+    res.status(404).send("User not found for id " + userID);
+  }
+  else {
+    user.splice(userID-1, 0);
+    res.status(204).send({'message' : 'User deleted'});
+  }
 });
 
 //delete to remove all the reminders from a given user
 app.delete('/users/:userId/reminders', function (req,res){
-  user.findById(req.params.userId, function(err, user) {
-    if (err) {
-      res.status(404).send(err);
-    }
-    else {
-      user.reminder.remove({_id : req.params.userId}, function (err, user) {
-        if (err) {
-          res.status(404).send(err);
-        }
-        else {
-          res.status(204).send({message : 'Reminders deleted'});
-        }
-      });
-    }
-  });
+  var userID = req.params.userId
+  if (user[userID-1]) {
+    res.status(404).send("User not found for id " + userID);
+  }
+  else {
+    user[userID-1].reminders = [];
+    res.status(204).send({'message' : 'Reminders deleted'});
+  }
 });
 
 //delete to remove one specific reminder at given user and reminder
 app.delete('/users/:userId/reminders/reminderId', function (req,res){
-  user.findById(req.params.userId, function(err, user) {
-    if (err) {
-      res.status(404).send(err);
+  var userID = req.params.userId;
+  var reminderID = req.params.reminderId;
+    if (!user[userID-1].remind[reminderID-1]) {
+      res.status(404).send("Reimder not found for id " + reminderID);
     }
     else {
-      user.reminder.pull(req.params.reminderId);
-      user.save(function(err, user) {
-        if (err) {
-          res.status(404).send(err);
-        }
-        else {
+      user[userID-1].remind.splice(reminderID-1, 0);
           res.status(204).send({message : 'Reminder deleted'});
-        }
-      });
     }
-  });
 });
 
 //listen to let user know the app is running on port 3000
